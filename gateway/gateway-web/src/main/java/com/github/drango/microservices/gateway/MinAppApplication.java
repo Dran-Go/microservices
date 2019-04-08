c3a5e1ed4068856eb4c1e8c1f502643803ab5f5b
dran-go <103636933@qq.com>
2019-04-08 22:44:25 +0800

    创建gateway服务

---
+++ gateway/gateway-web/src/main/java/com/github/drango/microservices/gateway/MinAppApplication.java
@@ -0,0 +1,17 @@
+package com.github.drango.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = "com.github.drango.microservices")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.github.drango.microservices")
+public class MinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinAppApplication.class, args);
+    }
+
+}
