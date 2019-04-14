6d69b0ec3f0c05870b3a4cd633aec48f748cfb5e
DranGo <103636933@qq.com>
2019-04-14 23:46:52 +0800

    完善发信逻辑

--- mail-server/app.py
+++ mail-server/app.py
@@ -4,22 +4,41 @@ from configuration import *
 
 app = Flask(__name__)
 
-
+# Sidecar状态监控
 @app.route("/health")
 def health():
     result = {'status': 'UP'}
     return Response(json.dumps(result), mimetype='application/json')
 
-
+# 发送邮箱地址校验邮件
 @app.route("/api/mail/verify", methods=['POST'])
-def send_mail():
+def send_verify_mail():
     email = request.form['email'].strip()
     username = request.form['username'].strip()
     verify_url = request.form['verifyUrl'].strip()
     subject = '您的帐户－请确认您的电子邮件地址'
-    with open('./templates/mail/VerifyTemplate.html', 'r', encoding='UTF-8') as f:
+    with open('./templates/mail/verify.html', 'r', encoding='UTF-8') as f:
         message = f.read()
     message = message.replace('{% username %}', username).replace('{% verify_url %}', verify_url)
+    msg = Message(
+        subject=subject,
+        recipients=[email],
+        html=message
+    )
+    mail.send(msg)
+    result = {'code': 200, 'message': 'ok', 'data': True}
+    return Response(json.dumps(result), mimetype='application/json')
+
+# 发送通信邮件
+@app.route("/api/mail/letter", methods=['POST'])
+def send_letter_mail():
+    email = request.form['email'].strip()
+    from_username = request.form['from_username'].strip()
+    text = request.form['text'].strip()
+    subject = str.format('{}给您发来一封新邮件', from_username)
+    with open('./templates/mail/letter.html', 'r', encoding='UTF-8') as f:
+        message = f.read()
+    message = message.replace('{% from_username %}', from_username).replace('{% text %}', text)
     msg = Message(
         subject=subject,
         recipients=[email],
@@ -32,11 +51,14 @@ def send_mail():
 
 if __name__ == '__main__':
     # 使用consul做为配置中心
-    SIDECAR_PORT = 8830
-    CONFIGURATION_KEY = "config/mail-server"
-    app_config = Consul(SIDECAR_PORT).get_configuration(CONFIGURATION_KEY)
+    try:
+        app_config = Consul(8830, "config/mail-server").get_configuration()
+    except:
+        print("Error: Loading configuration from consul failed, try to load locally.")
+        with open('./application.yml', 'r') as f:
+            app_config = yaml.safe_load(f)
 
-    # EMAIL SETTINGS
+    # 配置邮箱
     app_mail_config = app_config['mail']
     app.config.update(
         DEBUG=False,
